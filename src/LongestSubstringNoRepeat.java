09ba554b68d77e56d312d01ceb263ec56807751a
ジャパル ジャルキンベック <jarkynbyek@rismon.local>
2022-01-08 16:47:17 +0900

    solved with sliding window method

--- src/LongestSubstringNoRepeat.java
+++ src/LongestSubstringNoRepeat.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringNoRepeat {
     public static void main(String[] args){
-        System.out.println(lengthOfLongestSubstring(""));
-        System.out.println(lengthOfLongestSubstring("p"));
+//        System.out.println(lengthOfLongestSubstring(""));
+//        System.out.println(lengthOfLongestSubstring("p"));
         System.out.println(lengthOfLongestSubstring("dvdf"));
         System.out.println(lengthOfLongestSubstring("abcabcbb"));
         System.out.println(lengthOfLongestSubstring("bbbbb"));
@@ -9,23 +12,15 @@ public class LongestSubstringNoRepeat {
     }
 
     public static int lengthOfLongestSubstring(String s){
-        if (s.length() < 2) return s.length();
-
-        int longest = 1;
-        String longestSubString;
-        for(int i=0;i<s.length() - 1;i++){
-            longestSubString = String.valueOf(s.charAt(i));
-            for(int j=i+1; j<s.length(); j++){
-                if (longestSubString.indexOf(s.charAt(j)) == -1){
-                    longestSubString += s.charAt(j);
-                    if (longestSubString.length() > longest)
-                        longest = longestSubString.length();
-                }
-                else
-                    break;
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> m = new HashMap<>();
+        for(int j=0, i=0; j<n; j++){
+            if(m.containsKey(s.charAt(j))){
+                i = Math.max(m.get(s.charAt(j)), i);
             }
-
+            m.put(s.charAt(j), j + 1);
+            ans = Math.max(j - i + 1, ans);
         }
-        return longest;
+        return ans;
     }
 }
